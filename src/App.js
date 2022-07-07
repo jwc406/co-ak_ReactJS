d52da099f62f18fb5003b234190a63871cf95646
Gardenii <77850103+jwc406@users.noreply.github.com>
2022-07-07 17:54:44 +0900

    #11 json-server, REST API 까지
    json server - 작은 프로젝트에 적합
    
    REST API란?
    C : POST
    R : GET
    U : PUT
    D : DELETE 메소드 사용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import DayList from "./components/DayList";
 import Header from "./components/Header";
 import Day from "./components/Day";
+import EmptyPage from "./components/EmptyPage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <DayList />
+          </Route>
+          <Route path="/day/:day">
+            <Day />
+          </Route>
+          <Route>
+            <EmptyPage />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
